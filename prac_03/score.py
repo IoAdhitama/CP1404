15435a88d59ed81a66ebe2ed7a69adfb4124184c
Io <spicyicedtea@gmail.com>
2018-11-20 11:15:34 +0800

    Modify temperatures and score in prac_03 according to instructions

--- prac_03/score.py
+++ prac_03/score.py
@@ -4,13 +4,22 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    if score < 50:
-        print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    score_judge = score_decision(score)
+    print(score_judge)
+
+
+def score_decision(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        if score < 50:
+            return "Bad"
+
+
+main()
